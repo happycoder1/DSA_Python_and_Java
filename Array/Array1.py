f9bce5067a4b54da2afe7fdbda9e22329ef176bf
Dhruval <22280539+Dhruval10@users.noreply.github.com>
2021-10-30 13:21:31 -0700

    Create Array1.py

---
+++ Array/Array1.py
@@ -0,0 +1,16 @@
+from array import *
+
+array1 = array('i', [10,20,30,40,50])
+'''
+We can access each element of an array using the index of the element. The below code shows how to access an array element.
+'''
+print (array1[0])
+
+print (array1[2])
+'''
+Output:
+
+When we compile and execute the above program, it produces the following result, which shows the element is inserted at index position 1.
+10
+30
+'''
